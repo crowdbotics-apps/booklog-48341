db6ba990d91b62153bbc5ea928f602095a6d7f1c
Crowdbotics <team@crowdbotics.com>
2024-06-10 16:47:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "booklog-48341",
+  url: "https://booklog-48341.botics.co",
+  api: "https://booklog-48341.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
